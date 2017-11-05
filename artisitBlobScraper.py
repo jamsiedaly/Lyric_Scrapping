d15c26e35acee083b637c4f480a71553b9a1b331
jamsiedaly <1415278@studentmail.ul.ie>
2017-11-05 15:39:06 +0000

    added blobs

---
+++ artisitBlobScraper.py
@@ -0,0 +1,73 @@
+from bs4 import BeautifulSoup
+import urllib.request
+import os
+from lyricBlobs import lyricBlobs
+
+artist_name = 'kendrick_lamar/'
+artist_URL = 'http://www.lyricsfreak.com/k/kendrick+lamar/'
+
+
+def new_song(song_file):
+		song_file.write('\r\n\r\n'+'')
+
+def scrap_artist(artist_name, artist_URL):
+	domain = 'http://www.lyricsfreak.com'
+	loop = 0
+	opener = urllib.request.build_opener()
+	opener.addheaders = [('User-Agent', 'Mozilla/5.0')]
+	song_page = opener.open(artist_URL)
+	song_soup = BeautifulSoup(song_page.read(), 'html.parser')
+	lyrics = song_soup.find('table', {'name':'song'})
+	song_links = lyrics.find_all('a')
+	scraper = lyricBlobs()
+	song_file = scraper.create_files("plain_text", " of ", artist_name)
+	for link in song_links:
+		href = link['href']
+		if href != 'javascript:void(0)':
+			new_song(song_file)
+			print(href)
+			loop += 1
+			link_address = domain + href
+			try:
+				scraper.scrap_song(link_address, str(loop), artist_name, song_file)
+			except:
+				pass
+	
+artist_names = ['kendrick_lamar/',
+				'aesop_rock/',
+				'j_cole/',
+				'scroobius_pip/',
+				'lil_wayne/',
+				'tupac/',
+				'nas/',
+				'dre/',
+				'eminem/',
+				'kanye/',
+				'rakim/',
+				'outkast/',
+				'fabolous/',
+				'busta_ryhmes/',
+				'jay-z']
+
+artist_URLs = [	'http://www.lyricsfreak.com/k/kendrick+lamar/',
+				'http://www.lyricsfreak.com/a/aesop+rock/',
+				'http://www.lyricsfreak.com/j/j+cole/',
+				'http://www.lyricsfreak.com/d/dan+le+sac+vs+scroobius+pip/',
+				'http://www.lyricsfreak.com/l/lil+wayne/',
+				'http://www.lyricsfreak.com/t/tupac+shakur/',
+				'http://www.lyricsfreak.com/n/nas/',
+				'http://www.lyricsfreak.com/d/dr+dre/',
+				'http://www.lyricsfreak.com/e/eminem/',
+				'http://www.lyricsfreak.com/k/kanye+west/',
+				'http://www.lyricsfreak.com/r/rakim/',
+				'http://www.lyricsfreak.com/o/outkast/',
+				'http://www.lyricsfreak.com/f/fabolous/',
+				'http://www.lyricsfreak.com/b/busta+rhymes/',
+				'http://www.lyricsfreak.com/j/jay+z/']
+
+for i in range(0,100):
+	for i, artist in enumerate(artist_names):
+		try: 
+			scrap_artist(artist, artist_URLs[i])
+		except:
+			scrap_artist(artist, artist_URLs[i])
