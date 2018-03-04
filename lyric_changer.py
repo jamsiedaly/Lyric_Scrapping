d235037d3b01c7ac6442607603505718b03991c4
jamsiedaly <1415278@studentmail.ul.ie>
2018-03-04 22:12:25 +0000

    now creates paragraphs

--- lyric_changer.py
+++ lyric_changer.py
@@ -1,20 +1,44 @@
-import os
+import re
 
 
 def main():
     artist_name = 'eminem'
     directory = artist_name + '/'
-    for filename in os.listdir(directory):
-        file_type = filename[-3:]
-        if file_type == 'txt':
-            read_file(filename)
+    text_file = directory + 'raw.txt'
+    paragraphs = create_paragraphs(text_file)
+    refined_paragraphs = refine_paragraphs(paragraphs)
+    print paragraphs[5]
 
 
-def read_file(filename):
+def create_paragraphs(filename):
     my_file = open(filename, 'r')
+    paragraph_count = 0
+    paragraphs = []
+    new_paragraph = []
     for line in my_file.readlines():
-        if line[1:] == '(' and line[-1:] == ')':
-            continue
+        if valid_line(line):
+            line = line.strip()
+            if line:
+                new_paragraph.append(line)
+            elif new_paragraph:
+                paragraphs.append(new_paragraph)
+                new_paragraph = []
+    return paragraphs
+
+def refine_paragraphs(paragraphs):
+    print 'nice'
+
+
+
+def valid_line(line):
+    valid = True
+    expression = re.compile('.*x\d+', re.IGNORECASE)
+    result = expression.search(line)
+    if result is not None:
+        valid = False
+    if line[:1] == '(':
+        valid = False
+    return valid
 
 
 if __name__ == '__main__':
